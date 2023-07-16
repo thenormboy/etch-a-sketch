836e045f6e1ca948f917a47579953ece33a00e53
Bleaze Perris <bperriscode@gmail.com>
2023-07-15 21:02:13 -0400

    added sketch border and fit each pixel in the container

--- sketch.js
+++ sketch.js
@@ -1,12 +1,11 @@
 const gridSize = 16;
 const container = document.querySelector('.container');
+let pixelSize = 600 / gridSize;
 
 for (i = 0; i < gridSize; i++) {
     for (j = 0; j < gridSize; j++) {
         const content = document.createElement('div');
         content.classList.add('content');
-        content.style.padding = '20px';
-        content.style.border = '2px solid black';
 
         container.appendChild(content);
     }
