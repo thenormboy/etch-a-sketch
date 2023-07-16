3bf349e013dce32b4404a74e881c17eb2da73267
Bleaze Perris <bperriscode@gmail.com>
2023-07-15 21:16:51 -0400

    pixel fits within the container regardless of grid dimensions

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 const gridSize = 16;
 const container = document.querySelector('.container');
-let pixelSize = 600 / gridSize;
+let pixelSizePercent = (1 / gridSize) * 100;
 
 for (i = 0; i < gridSize; i++) {
     for (j = 0; j < gridSize; j++) {
         const content = document.createElement('div');
         content.classList.add('content');
+        content.style.width = pixelSizePercent + '%';
+        content.style.height = pixelSizePercent + '%';
 
         container.appendChild(content);
     }
